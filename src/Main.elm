430e60ec59d31d2353d49b1a6bf4b801dcbbb557
Bruno Oliveira <brunohdbass@gmail.com>
2019-02-10 13:08:33 -0200

    finish the crud

--- src/Main.elm
+++ src/Main.elm
@@ -2,10 +2,15 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (Html, Attribute, text, div, h1, h2, p, ul, li, input, label, button)
-import Html.Attributes exposing (src, class, value, type_, name, checked)
+import Html.Attributes exposing (src, class, value, type_, name, checked, classList)
 import Html.Events exposing (onInput, onClick, keyCode, on, targetValue)
 import Debug exposing (log)
 import Json.Decode as Json
+import Task
+import Random
+import Random.Char
+import Random.String
+import List.Extra
 
 ---- PROGRAM ----
 
@@ -21,24 +26,41 @@ main =
 
 ---- MODEL ----
 
-type Stage = Todo | Doing | InReview | Done
+type Stage = Todo | Doing | InReview | Done | UndefinedStage
 
-type alias Task
-  = { name: String
-    , stage: Stage
-    }
+type Error = No | TaskNotFound String
+
+type alias Task =
+  { name: String
+  , stage: Stage
+  , id: String
+  }
+
+type alias TaskToBe =
+  { name: String
+  , stage: Stage
+  }
+
+type alias Modal =
+  { open: Bool }
 
 type alias Model =
   { tasks: List Task
-  , taskToBe: Task
+  , taskToBe: TaskToBe
+  , modal: Modal
+  , editing: Maybe Task
+  , error: Error
   }
 
 
 init : ( Model, Cmd Msg )
 init =
   (
-    { tasks = [{ name = "Make Soda", stage = Todo }]
+    { tasks = []
     , taskToBe = { name = "", stage = Todo }
+    , modal = Modal False
+    , editing = Nothing
+    , error = No
     }
   , Cmd.none)
 
@@ -46,24 +68,51 @@ init =
 
 ---- UPDATE ----
 
-
 type Msg
-  = SetName String
+  = NoOp
+  | SetName String
   | SetStage Stage
-  | Save
+  | ToggleModal
+  | Save String
+  | Create
+  | StartEditing String
+  | Delete Task
+  | Edit (Maybe Task) Stage
 
-setName : String -> Task -> Task
+-- Update Helpers
+setName : String -> TaskToBe -> TaskToBe
 setName name task = 
   { task | name = name }
 
-setStage : Stage -> Task -> Task
+setStage : Stage -> TaskToBe -> TaskToBe
 setStage stage task = 
   { task | stage = stage }
 
+toggleModal : Modal -> Modal
+toggleModal modal = 
+  { modal | open = not modal.open }
 
+saveNewTask : Cmd Msg
+saveNewTask = 
+  Random.generate Save (Random.String.string 30 Random.Char.latin)
+
+updateTaskStage : (Maybe Task) -> Stage -> Task -> Task
+updateTaskStage maybeTask stage task = 
+  case maybeTask of
+    Just {id} -> 
+      if id == task.id then
+        { task | stage = stage }
+      else
+        task
+    Nothing ->
+      task
+
+-- REDUCER
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
+    NoOp ->
+      (model, Cmd.none)
     SetName name ->
       (
         { model
@@ -82,25 +131,50 @@ update msg model =
         }
         , Cmd.none
       )
-    Save ->
+    Create ->
+      (model, saveNewTask)
+    Save newId ->
       (
         { model
-      | taskToBe =
-        model.taskToBe
-        |> setStage Todo
-        |> setName ""
-        , tasks =
-          if String.isEmpty model.taskToBe.name then
-             model.tasks
+          | taskToBe =
+            model.taskToBe
+            |> setStage Todo
+            |> setName ""
+          , tasks =
+            if String.isEmpty model.taskToBe.name then
+               model.tasks
              else
-             model.tasks ++ [{ name = model.taskToBe.name, stage = model.taskToBe.stage }]
-           }
+               model.tasks ++ [{ name = model.taskToBe.name, stage = model.taskToBe.stage, id = newId }]
+         }
         , Cmd.none
       )
+    StartEditing id ->
+      let maybeTask = List.Extra.find (\task -> task.id == id) model.tasks
+      in case maybeTask of
+        Just task -> 
+          ({ model | editing = Just task, modal = Modal True }, Cmd.none)
+        Nothing ->
+          ({ model | error = TaskNotFound "You tried to update a task that doesn't exists" }, Cmd.none)
+    Edit maybeTask stage ->
+      ({ model
+          | editing = Nothing
+          , modal = Modal False
+          , tasks = List.map (updateTaskStage maybeTask stage) model.tasks
+        }
+      , Cmd.none
+      )
+    Delete task -> 
+      ( { model
+          | tasks = model.tasks |> List.filter (\t -> t.id /= task.id)
+          , modal = Modal False
+        }
+      , Cmd.none
+      )
+    ToggleModal -> 
+      ( { model | modal = model.modal |> toggleModal }, Cmd.none )
+      
 
----- VIEW ----
-
-
+-- MAIN VIEW
 view : Model -> Html Msg
 view model = 
   viewBox 
@@ -113,7 +187,7 @@ view model =
   , div [ class "form" ]
     [ input
       [ onInput SetName
-      , onEnter Save 
+      , onEnter Create 
       , value model.taskToBe.name
       , class "form__input"
       ] []
@@ -122,11 +196,73 @@ view model =
       , viewStageCheckbox (model.taskToBe.stage == Doing) (SetStage Doing) (translateStage Doing)
       , viewStageCheckbox (model.taskToBe.stage == InReview) (SetStage InReview) (translateStage InReview)
       , viewStageCheckbox (model.taskToBe.stage == Done) (SetStage Done) (translateStage Done)
-      , button [ class "form__btn", onClick Save ] [ text "SAVE" ]
+      , button [ class "form__btn", onClick Create ] [ text "SAVE" ]
       ]
+    , viewModal model.modal model.editing model.error
     ]
   ]
 
+-- VIEWS
+
+viewModal : Modal -> (Maybe Task) -> Error -> Html Msg
+viewModal modal maybeTask error =
+  let (id, stage) = maybeTaskToIdStageTuple maybeTask
+  in div [ classList [("modal", True), ("modal--open", modal.open)] ]
+      [ div [ class "modal__dialog" ]
+          [ div [ class "modal__title" ] [ text ("CHANGE ACTUAL STAGE") ]
+          , div [ class "modal__subtitle" ] [ text ("Editing: " ++ id  ++ " in " ++ (translateStage stage)) ]
+          , viewError error
+          , button
+              [ classList [("modal__stage", True), ("modal__stage--selected", stage == Todo)]
+              , onClick (viewHandleStageClick Todo maybeTask)
+              ]
+              [ text <| translateStage <| Todo ]
+          , button
+              [ classList [("modal__stage", True), ("modal__stage--selected", stage == Doing)]
+              , onClick (viewHandleStageClick Doing maybeTask)
+              ]
+              [ text <| translateStage <| Doing ]
+          , button
+              [ classList [("modal__stage", True), ("modal__stage--selected", stage == InReview)]
+              , onClick (viewHandleStageClick InReview maybeTask)
+              ]
+              [ text <| translateStage <| InReview ]
+          , button
+              [ classList [("modal__stage", True), ("modal__stage--selected", stage == Done)]
+              , onClick (viewHandleStageClick Done maybeTask)
+              ]
+              [ text <| translateStage <| Done ]
+          , button [ class "modal__stage modal__stage--danger", onClick (viewHandleTaskDelete maybeTask) ] [ text "REMOVE TASK" ]
+          , button [ class "modal__close", onClick ToggleModal ] [ text "×" ]
+          ]
+       ]
+
+viewHandleStageClick : Stage -> Maybe Task -> Msg
+viewHandleStageClick newStage maybeTask =
+ case maybeTask of
+   Just task ->
+     if (newStage == task.stage) then
+        NoOp
+     else 
+        Edit maybeTask newStage
+   Nothing ->
+     NoOp
+
+viewHandleTaskDelete : Maybe Task -> Msg
+viewHandleTaskDelete maybeTask =
+ case maybeTask of
+   Just task ->
+     Delete task
+   Nothing ->
+     NoOp
+
+maybeTaskToIdStageTuple : Maybe Task -> (String, Stage)
+maybeTaskToIdStageTuple maybeTask =
+   case maybeTask of
+     Just task ->
+       (task.id, task.stage)
+     Nothing ->
+       ("NOTHING", UndefinedStage)
 
 viewBoardColumn : List Task -> Stage -> Html Msg
 viewBoardColumn tasks stage =
@@ -138,17 +274,13 @@ viewBoardColumn tasks stage =
         |> div []
     ]
 
-taskIs : Stage -> Task -> Bool
-taskIs stage task =
-  stage == task.stage
-
-translateStage : Stage -> String
-translateStage stage =
-  case stage of
-    Todo -> "Todo" 
-    Doing -> "Doing" 
-    InReview -> "In Review" 
-    Done -> "Done" 
+viewError : Error -> Html Msg
+viewError error =
+  case error of 
+    TaskNotFound msg ->
+      div [ class "alert alert--danger" ] [ text msg ]
+    No ->
+      div [] []
 
 viewStageCheckbox : Bool -> Msg -> String -> Html Msg
 viewStageCheckbox isChecked msg desc =
@@ -158,7 +290,7 @@ viewStageCheckbox isChecked msg desc =
       , name "stage" 
       , checked isChecked
       , onClick msg
-      , onEnter Save
+      , onEnter Create
       , class "form__stage-checkbox" 
     ] []
     , text desc
@@ -170,7 +302,9 @@ viewBox elements
 
 viewTodo : Task -> Html Msg
 viewTodo task
-  = div [ class "board__task" ] [text task.name]
+  = div [ class "board__task", onClick (StartEditing task.id) ] [text task.name]
+
+-- HELPERS
 
 onEnter : Msg -> Attribute Msg
 onEnter msg =
@@ -183,3 +317,15 @@ onEnter msg =
   in
      on "keydown" (Json.andThen isEnter keyCode)
 
+taskIs : Stage -> Task -> Bool
+taskIs stage task =
+  stage == task.stage
+
+translateStage : Stage -> String
+translateStage stage =
+  case stage of
+    Todo -> "Todo" 
+    Doing -> "Doing" 
+    InReview -> "In Review" 
+    Done -> "Done" 
+    UndefinedStage -> "" 
